3939409c562669b0d8ea59473400f6ef2db0a0ca
sadmanalam142 <sadmanalam142@gmail.com>
2024-06-03 22:00:05 +0600

    authentication done

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,7 +1,11 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
 const MainLayout = () => {
     return (
         <div>
-            
+            <Navbar></Navbar>
+            <Outlet></Outlet>
         </div>
     );
 };
---
+++ src/routes/routes.jsx
@@ -0,0 +1,35 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home";
+import ErrorPage from "../pages/ErrorPage";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        // loader: () =>
+        //   fetch("https://stride-final-project-server.vercel.app/shoes"),
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+    ],
+  },
+//   {
+//     path: "dashboard",
+//     element: <DashboardLayout />,
+//     errorElement: <ErrorPage />,
+//   },
+]);
