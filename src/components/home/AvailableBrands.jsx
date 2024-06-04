f2c6f43207bf45fa278cd048e8db26195f5a58a5
sadmanalam142 <sadmanalam142@gmail.com>
2024-06-04 15:56:47 +0600

    home section done

---
+++ src/components/home/AvailableBrands.jsx
@@ -0,0 +1,41 @@
+const AvailableBrands = () => {
+  return (
+    <div>
+        <h1 className="my-8 text-3xl font-bold text-center">Available Brands</h1>
+      <div className="grid grid-cols-3 gap-6 ml-28">
+      <div className="card w-48 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h1 className="text-2xl">Addidas</h1>
+        </div>
+      </div>
+      <div className="card w-48 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h1 className="text-2xl">Nike</h1>
+        </div>
+      </div>
+      <div className="card w-48 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h1 className="text-2xl">Puma</h1>
+        </div>
+      </div>
+      <div className="card w-48 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h1 className="text-2xl">SS</h1>
+        </div>
+      </div>
+      <div className="card w-48 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h1 className="text-2xl">Reebok</h1>
+        </div>
+      </div>
+      <div className="card w-48 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h1 className="text-2xl">SG</h1>
+        </div>
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default AvailableBrands;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import FrequentQuestions from "../components/home/FrequentQuestions";
 import Banner from "../components/home/Banner";
 import Products from "../components/home/Products";
+import AvailableBrands from "../components/home/AvailableBrands";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <AvailableBrands></AvailableBrands>
             <Products></Products>
             <FrequentQuestions></FrequentQuestions>
         </div>
